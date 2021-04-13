b8aadc5aa81aa4b5ba681c5d837c6828ada24f8b
Johan Griesel <jdgriesel@gmail.com>
2021-04-13 12:27:38 +0200

    Better readme and new camel case splitter function

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # dx-utils
+The Divblox utilities package provides a bunch of useful functions that are primarily used within the Divblox nodejs library. These functions can however be freely used in any other nodejs project
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const readline = require("readline");
 
 const dx_utils = {
+    //#region Command line functionality
     /**
      * Returns the user's input to the given command line question
      * @param question
@@ -15,6 +16,33 @@ const dx_utils = {
             rl.question(question+" ", answer => {rl.close();resolve(answer)})
         });
     },
+    //#endregion
+
+    //#region Timers
+    /**
+     * Sleeps for ms seconds when called with the await keyword
+     * @param ms
+     * @returns {Promise<unknown>}
+     */
+    async sleep(ms = 0) {
+        return await new Promise(resolve => setTimeout(resolve, ms));
+    },
+    //#endregion
+
+    //#region Numbers and math related
+    /**
+     * Returns the value as a float with [decimal_points] number of decimals
+     * @param value
+     * @param decimal_points
+     * @returns {number}
+     */
+    getValueToDecimal(value = 0,decimal_points = 0) {
+    const factor = Math.pow(10,decimal_points);
+    return Math.round(factor*value)/factor;
+},
+    //#endregion
+
+    //#region Strings and string manipulation
     /**
      * Returns a date string that can be used with MySQL
      * @param current_date_utc
@@ -27,23 +55,16 @@ const dx_utils = {
         const date_from_current_str = current_date_obj.toISOString().replace('T', ' ');
         return date_from_current_str.substring(0,date_from_current_str.length - 5);
     },
+
     /**
-     * Sleeps for ms seconds when called with the await keyword
-     * @param ms
-     * @returns {Promise<unknown>}
+     * Splits a camel case string into a lower case string, separated with a splitter character
+     * @param camel_case_str The string to split
+     * @param splitter The charactor to use as the splitter "_" or "-", etc
+     * @returns {string} The splitted string in lower case
      */
-    async sleep(ms = 0) {
-        return await new Promise(resolve => setTimeout(resolve, ms));
-    },
-    /**
-     * Returns the value as a float with [decimal_points] number of decimals
-     * @param value
-     * @param decimal_points
-     * @returns {number}
-     */
-    getValueToDecimal(value = 0,decimal_points = 0) {
-    const factor = Math.pow(10,decimal_points);
-    return Math.round(factor*value)/factor;
-}
+    getCamelCaseSplittedToLowerCase(camel_case_str = '',splitter = "_") {
+        return camel_case_str.replace(/([a-z0-9])([A-Z0-9])/g, '$1'+splitter+'$2').toLowerCase();
+    }
+    //#endregion
 }
 module.exports = dx_utils;
